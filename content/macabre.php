3620bfb88ba7d0e35d35371fe2395649c7fbad86
Pekko Lipsanen <pekko.lipsanen@iki.fi>
2014-01-30 15:38:52 +0100

    too old PHP for array shorthand notation

--- content/macabre.php
+++ content/macabre.php
@@ -9,21 +9,21 @@ Tervetuloa lumoutumaan Danse Macabresta!
 
 <table id="showtimes">
     <?php 
-    $shows = [
-        ["17.3.", "Aleksanterin teatteri", "Helsinki"],
-        ["19.3.", "Hällä-teatteri", "Tampere"],
-        ["21.3.", "Pohjankartanon juhlasali", "Oulu"],
-        ["25.3.", "Aleksanterin teatteri", "Helsinki"],
-        ["24.3.", "Aleksanterin teatteri", "Helsinki"],
-        ["23.3.", "Aleksanterin teatteri", "Helsinki"],
-        ["28.3.", "Louhisali", "Espoo"],
-        ["29.3.", "Louhisali", "Espoo"],
-        ["30.3.", "Louhisali", "Espoo"],
-        ["8.4.",  "Aleksanterin teatteri", "Helsinki"],
-        ["7.4.",  "Aleksanterin teatteri", "Helsinki"],
-        ["12.4.", "Sigyn-sali", "Turku"],
-        ["14.4.", "Kaupunginteatteri", "Lappeenranta"],
-    ];
+    $shows = array(
+        array("17.3.", "Aleksanterin teatteri", "Helsinki"),
+        array("19.3.", "Hällä-teatteri", "Tampere"),
+        array("21.3.", "Pohjankartanon juhlasali", "Oulu"),
+        array("25.3.", "Aleksanterin teatteri", "Helsinki"),
+        array("24.3.", "Aleksanterin teatteri", "Helsinki"),
+        array("23.3.", "Aleksanterin teatteri", "Helsinki"),
+        array("28.3.", "Louhisali", "Espoo"),
+        array("29.3.", "Louhisali", "Espoo"),
+        array("30.3.", "Louhisali", "Espoo"),
+        array("8.4.",  "Aleksanterin teatteri", "Helsinki"),
+        array("7.4.",  "Aleksanterin teatteri", "Helsinki"),
+        array("12.4.", "Sigyn-sali", "Turku"),
+        array("14.4.", "Kaupunginteatteri", "Lappeenranta"),
+    );
     foreach($shows as $s) {
         echo '
         <tr>
