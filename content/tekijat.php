518bc46f2891ea1a17136585b3ff66cd1ee4e0db
okalintu <ilkka.oksanen@aalto.fi>
2014-02-02 20:06:40 +0200

    added tekijat.php and styles for it

--- content/tekijat.php
+++ content/tekijat.php
@@ -1,1 +1,306 @@
-<p>Tekijät TODO</p>
+<div id="col_container">
+    <div id="col1" class="makers">
+    </div>
+    <div id="col2" class="makers">
+    </div>
+    <div id="col3" class="makers">
+    </div>
+    <div id="col4" class="makers">
+    </div>
+</div>
+<div id="maker_container" class="hidden">
+    <div class="team" id="Ohjaaja">
+    Tapio Kankaanpää
+    </div>
+    <div class="team" id="Apuohjaaja">
+    Ville Saukko
+    </div>
+    <div class="team" id="Tuottajat">
+    Essi Puustinen
+    Iida Sormanen
+    Saara Matala
+    </div>
+    <div class="team" id="Käsikirjoitus">
+    Saara Honkanen
+    Vili Nissinen
+    </div>
+    <div class="team" id="Näyttelijät">
+    Eero Tiilikainen
+    Harri Sarsa
+    Iita Kejonen
+    Jari Vepsäläinen
+    Juha-Matti Kahilakoski
+    Kirsti Larja
+    Mika Kuokkanen
+    Mikko Laine
+    Pipsa Haavisto
+    Saara Hakkarainen
+    Tuure Parkkinen
+    </div>
+    <div class="team" id="Tanssijat">
+    Viivi Mattila
+    Iiro Perttula
+    Oona Jalkanen
+    Pihla Melander
+    Saara Kaija
+    Sami Harju
+    Sofia Ahanen
+    Venla Keskinen
+    </div>
+    <div class="team" id="Koreografit">
+    Anna Äimälä
+    Anna-Maria Lahesmaa-Korpinen
+    Duy Doan
+    Hanna-Mari Ikonen
+    Ilona Kuusela
+    Miia Puranen
+    Nora Wind
+    Olli Saarikivi
+    Oona Jalkanen
+    Pihla Melander
+    Sami Harju
+    Sofia Ahanen
+    Venla Keskinen
+    Viivi Mattila
+    </div>
+    <div class="team" id="Orkesteri">
+    Hannu Teittinen
+    Joppe Rauhamaa
+    Arla von Konow
+    Clemens Westrup
+    Hanna Partanen
+    Heikki Kauppinen
+    Heikki Vallinoja
+    Jaakko Kiuru
+    Joel Vuohelainen
+    Roosa Silaste
+    Saara Matala
+    Severi Vapalahti
+    </div>
+    <div class="team" id="Sovitus">
+    Marko Rintamäki
+    Aki Ali-Vehmas
+    Aku Rouhe
+    Christopher Helle
+    Hannu Teittinen
+    Heikki Vallinoja
+    Henri Pihkala
+    Iita Kejonen
+    Jaakko Kiuru
+    Joonas Vanninen
+    Joppe Rauhamaa
+    Juha Vanhalakka
+    Juuso Jaatinen
+    Minni Pirttimaa
+    Nina Forsman
+    Olli Keskinen
+    Pia Niemi
+    Roosa Silaste
+    Saara Matala
+    Severi Vapalahti
+    Toni Mäkelä
+    </div>
+    <div class="team" id="Sanoitus">
+    Harri Sarsa
+    Heikki Moilanen
+    Aku Rouhe
+    Anna Törrönen
+    Antti Sykäri
+    Eevert Saukkokoski
+    Iita Kejonen
+    Jani Kettunen
+    Jari Vepsäläinen
+    Johannes Lehto
+    Leea Riittinen
+    Liisa Itkonen
+    Linnea Löytönen
+    Mikko Laine
+    Noora Tujunen
+    Ossi-Matti Lahtinen
+    Rosa Nylén
+    Saana Lipasti
+    Saara Hakkarainen
+    Saara Matala
+    Tyko Viertiö
+    Vili Nissinen
+    Ville Saukko
+    </div>
+    <div class="team" id="Puvustus">
+    Riina Nieminen
+    Saara Honkanen
+    Stella Vahteristo
+    Ainotuuli Vaittinen
+    Anna Äimälä
+    Anna-Maria Lahesmaa-Korpinen
+    Erkko Eeva 
+    Jenna Kaisanlahti
+    Jenny Manninen
+    Johannes Lehto
+    Kaisla Väre
+    Laura Kainulainen
+    Laura Zein
+    Linda Hyrkkänen
+    Linnea Löytönen
+    Marianna Mattila
+    Moona Kantele
+    Nora Wind
+    Oona Korhonen
+    Pia Niemi
+    Satu Sahlstedt
+    Sini Vehviläinen
+    Sonja Palomäki
+    Susanne Lindholm
+    Suvi Lehikoinen
+    Teresia Borgman
+    Valeryia Pulko  
+    </div>
+    <div class="team" id="Maskeeraus">
+    Tuuli Klemetti
+    Alexandra Zühlke
+    Heidi Välisalmi
+    Johanna Pennanen
+    Kaisu Palomäki
+    Oona Korhonen
+    Susanna Mikkola
+    </div>
+    <div class="team" id="Lavastus">
+    Sami Laakso
+    Hanna Skoglund
+    Jani Kautto
+    Jari Vepsäläinen
+    Johannes Lehto
+    Juho Leinonen
+    Matti-Pekka Laaksonen
+    Susanna Mikkola
+    Tuomas Mikkonen
+    Valeryia Pulko
+    </div>
+    <div class="team" id="Rekvisiitta">
+    Juha "Bube" Anttila
+    Evgenia Antonova
+    Hanna Skoglund
+    Jani Kautto
+    Jari Vepsäläinen
+    Johannes Lehto
+    Juho Leinonen
+    Kaisla Väre
+    Liisa Itkonen
+    Patrick Jensen
+    Saana Lipasti
+    Susanna Mikkola
+    Susanne Lindholm
+    Vilma Uusi-Äijö
+    </div>
+    <div class="team" id="Näyttämömestarit">
+    Henri Hyttinen
+    Heta Närhi
+    </div>
+    <div class="team" id="Valotekniikka">
+    Arttu Voutilainen
+    Oskari Akkanen
+    Pekka Lammi
+    Timo Hiekkanen
+    Torsti Larja
+    Valtteri Martikainen
+    </div>
+    <div class="team" id="Äänitekniikka">
+    Jaakko Vallinoja
+    Kristian Penttinen
+    Meea Kemppainen
+    Veikka Kähkönen
+    </div>
+    <div class="team" id="AD-tiimi">
+    Liinu Kivioja
+    Heidi Välisalmi
+    Henri Pihkala
+    Ida Salonen
+    Johannes Lehto
+    Juha Anttila
+    Kia Lehti
+    </div>
+    <div class="team" id="Dokumentointi">
+    Henri Pihkala
+    Jarkko Lamminsalo
+    Joel Pirttimaa
+    Johannes Lehto
+    Leea Riittinen
+    </div>
+    <div class="team" id="Markkinointi">
+    Meri Terho
+    Anna Nermes
+    Eeva Tervahartiala
+    Henri Hyttinen
+    Kaarlo Väisänen
+    </div>
+    <div class="team" id="Yrityssuhteet">
+    Oliver Kollberg
+    Aarne Leinonen
+    Essi Puustinen
+    Harri Sarsa
+    Henri Hyttinen
+    Kaarlo Väisänen
+    </div>
+    <div class="team" id="Lipunmyynti ja lippukauppa">
+    Aino Kuhmonen
+    Ilkka Oksanen
+    Pekko Lipsanen
+    </div>
+    <div class="team" id="Hupitiimi">
+    Aiju Heinonen
+    Minni Pirttimaa
+    Antti Väisänen
+    Christopher Helle
+    Evgenia Antonova
+    Henri Pihkala
+    Joel Pirttimaa
+    Juha Anttila
+    Juha Lahti
+    Juho Leinonen
+    Kia Lehti
+    Luiza Sayfullina
+    Niko Ferm
+    Noora Tujunen
+    Pipsa Haavisto
+    Sami Laakso
+    Susanna Mikkola
+    Veera Narvo
+    Vilma Uusi-Äijö
+    </div>
+    <div class="team" id="Kiertueblondit">
+    Elisa Terho
+    Aki Ali-Vehmas
+    Aleksi Rinta-Kauppila
+    Antti Heikkilä
+    Ilona Kuusela
+    Kia Lehti
+    Pia Niemi
+    Tuomas Mikkonen
+    Veera Narvo
+    </div>
+    <div class="team" id="Kiertuepaikkakuntien natiivit">
+    Tampere: Terhi Alaviitala
+    Oulu: Sauli Viinamäki
+    Turku: Jesse Grönlund
+    Lappeenranta:
+    </div>
+
+</div>
+<script>
+var column = 1;
+var teams = $('.team');
+teams.each(function(idx,item)
+{
+    if (item.id=='Sovitus' || item.id=='Maskeeraus' || item.id=='Markkinointi')
+    {
+        column++;
+    }
+    var col = $('#col'+column);
+    col.append('<div class="teamheader">'+item.id+'</div>');
+
+    var makers = item.innerHTML.split('\n');
+    for (var i=0;i<makers.length;i++)
+    {
+        col.append(makers[i]+'<br>')
+    }
+});
+</script>
