2c2f641c38aeadf50551b4ae6d431ed2e7028075
okalintu <ilkka.oksanen@aalto.fi>
2014-02-03 01:14:46 +0200

    hahmogallerian virittelyä

--- content/hahmot.php
+++ content/hahmot.php
@@ -1,1 +1,15 @@
-<p>Hahmot TODO</p>
+<div id="framecontainer">
+<canvas id="gallery" width="460" height="660"></canvas>
+<script>
+    var canvas = document.getElementById('gallery');
+    var context = canvas.getContext('2d');
+    var frame= new Image();
+    var character = new Image();
+    var xoffset=30;
+    var yoffset=30; 
+    frame.onload = function(){context.drawImage(frame,0,0);}; 
+    character.onload = function(){context.drawImage(character,xoffset,yoffset);};
+    frame.src = 'img/frame.png'
+    character.src = 'img/kardinaali.jpg';
+</script>
+</div>
