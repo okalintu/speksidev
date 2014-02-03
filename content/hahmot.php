cee784a2c15077ccdfc0a7de0405caa5b8273006
okalintu <ilkka.oksanen@aalto.fi>
2014-02-03 19:15:29 +0200

    resized featherimages

--- content/hahmot.php
+++ content/hahmot.php
@@ -2,81 +2,81 @@
     <script type="text/javascript" src="fancybox/jquery.fancybox.pack.js?v=2.1.5"></script>
     
     <div class="thumb_container">
-        <a class="thumbnail" href="#ylii" rel="g"><img src="img/ylii_sulka.png"/></a>
-        <a class="thumbnail" href="#kardinaali" rel="g"><img src="img/kardinaali_sulka.png"/></a>
-        <a class="thumbnail" href="#aaliyah" rel="g"><img src="img/aaliyah_sulka.png"/></a>
-        <a class="thumbnail" href="#perdita" rel="g"><img src="img/perdita_sulka.png"/></a>
-        <a class="thumbnail" href="#venus" rel="g"><img src="img/venus_sulka.png"/></a>
-        <a class="thumbnail" href="#julian" rel="g"><img src="img/julian_sulka.png"/></a>
-        <a class="thumbnail" href="#armand" rel="g"><img src="img/armand_sulka.png"/></a>
-        <a class="thumbnail" href="#valmont" rel="g"><img src="img/valmont_sulka.png"/></a>
-        <a class="thumbnail" href="#incubus" rel="g"><img src="img/incubus_sulka.png"/></a>
-        <a class="thumbnail" href="#succubus" rel="g"><img src="img/succubus_sulka.png"/></a>
-        <a class="thumbnail" href="#antabus" rel="g"><img src="img/antabus_sulka.png"/></a>
+        <a class="thumbnail" href="#ylii" rel="g"><img src="img/hahmot/ylii_sulka.png"/></a>
+        <a class="thumbnail" href="#kardinaali" rel="g"><img src="img/hahmot/kardinaali_sulka.png"/></a>
+        <a class="thumbnail" href="#aaliyah" rel="g"><img src="img/hahmot/aaliyah_sulka.png"/></a>
+        <a class="thumbnail" href="#perdita" rel="g"><img src="img/hahmot/perdita_sulka.png"/></a>
+        <a class="thumbnail" href="#venus" rel="g"><img src="img/hahmot/venus_sulka.png"/></a>
+        <a class="thumbnail" href="#julian" rel="g"><img src="img/hahmot/julian_sulka.png"/></a>
+        <a class="thumbnail" href="#armand" rel="g"><img src="img/hahmot/armand_sulka.png"/></a>
+        <a class="thumbnail" href="#valmont" rel="g"><img src="img/hahmot/valmont_sulka.png"/></a>
+        <a class="thumbnail" href="#incubus" rel="g"><img src="img/hahmot/incubus_sulka.png"/></a>
+        <a class="thumbnail" href="#succubus" rel="g"><img src="img/hahmot/succubus_sulka.png"/></a>
+        <a class="thumbnail" href="#antabus" rel="g"><img src="img/hahmot/antabus_sulka.png"/></a>
     </div>
 
     <div id="incubus" class="pic" style="display:none">
-        <img src="img/incubus.jpg" />
+        <img src="img/hahmot/incubus.jpg" />
         <div class="name">Incubus</div>
-        <div class="quote">"Ei meill‰ ole mik‰‰n kiire takaisin helvettiin."</div>
+        <div class="quote">"Ei meill√§ ole mik√§√§n kiire takaisin helvettiin."</div>
     </div>
 
     <div id="valmont" class="pic" style="display:none">
-        <img src="img/valmont.jpg" />
+        <img src="img/hahmot/valmont.jpg" />
         <div class="name">Valmont Vaillance</div>
         <div class="quote">"Antaudu lemmen hurmalle ja unohda sun murheesi!"</div>
     </div>
 
     <div id="aaliyah" class="pic" style="display:none">
-        <img src="img/aaliyah.jpg" />
+        <img src="img/hahmot/aaliyah.jpg" />
         <div class="name">Aaliyah Basheera</div>
         <div class="quote">"Me olemme kunniallisia naisia inkvisition asialla!"</div>
     </div>
 
     <div id="armand" class="pic" style="display:none">
-        <img src="img/armand.jpg" />
+        <img src="img/hahmot/armand.jpg" />
         <div class="name">Armand Enterlauper</div>
-        <div class="quote">"T‰ss‰ on pakko olla joku apostoli haudattuna."</div>
+        <div class="quote">"T√§ss√§ on pakko olla joku apostoli haudattuna."</div>
     </div>
 
     <div id="julian" class="pic" style="display:none">
-        <img src="img/julian.jpg" />
+        <img src="img/hahmot/julian.jpg" />
         <div class="name">Julian</div>
-        <div class="quote">"El‰m‰ metsurina ei ollut mua varten."</div>
+        <div class="quote">"El√§m√§ metsurina ei ollut mua varten."</div>
     </div>
 
     <div id="ylii" class="pic" style="display:none">
-        <img src="img/ylii.jpg" />
+        <img src="img/hahmot/ylii.jpg" />
         <div class="name">Yli-inkvisiittori</div>
         <div class="quote">"Voitte suudella sormustani."</div>
     </div>
 
     <div id="kardinaali" class="pic" style="display:none">
-        <img src="img/kardinaali.jpg" />
+        <img src="img/hahmot/kardinaali.jpg" />
         <div class="name">Kardinaali du Brillant</div>
         <div class="quote">"Ajattele minua pelastajanasi!"</div>
     </div>
 
     <div id="perdita" class="pic" style="display:none">
-        <img src="img/perdita.jpg" />
+        <img src="img/hahmot/perdita.jpg" />
         <div class="name">Dona Perdita Orgullo</div>
-        <div class="quote">"Manikyyri, pedikyyri, hiukset, korut... ja mit‰ me laitetaan p‰‰lle!"</div>
+        <div class="quote">"Manikyyri, pedikyyri, hiukset, korut... ja mit√§ me laitetaan p√§√§lle!"</div>
     </div>
 
     <div id="antabus" class="pic" style="display:none">
-        <img src="img/antabus.jpg" />
+        <img src="img/hahmot/antabus.jpg" />
         <div class="name">Antabus</div>
         <div class="quote">"Keittoo!"</div>
     </div>
 
     <div id="succubus" class="pic" style="display:none">
-        <img src="img/succubus.jpg" />
+        <img src="img/hahmot/succubus.jpg" />
         <div class="name">Succubus</div>
-        <div class="quote">"Voisimme olla enkeleit‰, jos niin haluat."</div>
+        <div class="quote">"Voisimme olla enkeleit√§, jos niin haluat."</div>
     </div>
 
     <div id="venus" class="pic" style="display:none">
-        <img src="img/venus2.jpg" />
+        <img src="img/hahmot/venus2.jpg" />
         <div class="name">Venus in Furs</div>
         <div class="quote">"Ahkera nainen asiakkaan nappaa."</div>
     </div>
