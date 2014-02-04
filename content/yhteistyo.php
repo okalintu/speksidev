0ecb01eb29dbac160bcc007a4d2378e816a75d71
speksi <speksi@cod.nodeta.fi>
2014-02-05 01:01:58 +0200

    reordered logos

--- content/yhteistyo.php
+++ content/yhteistyo.php
@@ -2,14 +2,16 @@
 header("Content-Type: text/html; charset=utf-8");
 ?>
 <div id="logot">
+	<h2 class="companies">Pääyhteistyökumppanit</h2>
 	<a href="http://www.espoo.fi/"><img src="img/logot/logo_espoo_harmaa.png"></a>
-	<a href="http://julkku.fi/"><img src="img/logot/logo_julkku_harmaa.png"></a>
-	<a href="http://www.metsagroup.fi/"><img src="img/logot/logo_metsa_harmaa.png"></a><br>
-	<a href="http://www.okautorent.fi/"><img src="img/logot/logo_okrent_harmaa.png"></a>
-	<a href="http://tek.fi/"><img src="img/logot/logo_tek_harmaa.png"></a>
+	<a href="http://seepsula.fi/"><img src="img/logot/logo_seepsula_harmaa.png"></a>
 	<a href="http://www.yit.fi/"><img src="img/logot/logo_yit_harmaa.png"></a>
-	<a href="fi.capgemini.com"><img src="img/logot/logo_capgemini_harmaa.png"></a>
+	<h2 class="companies"> Yhteistyössä</h2>
+	<a href="http://www.metsagroup.fi/"><img src="img/logot/logo_metsa_harmaa.png"></a>
+	<a href="http://tek.fi/"><img src="img/logot/logo_tek_harmaa.png"></a>
+	<a href="http://fi.capgemini.com"><img src="img/logot/logo_capgemini_harmaa.png"></a>
+	<a href="http://julkku.fi/"><img src="img/logot/logo_julkku_harmaa.png"></a>
 	<a href="http://www.dhl.fi/"><img src="img/logot/logo_dhl_harmaa.png"></a>
 	<a href="http://www.roland.fi/"><img src="img/logot/logo_roland_harmaa.png"></a>
-	<a href="http://seepsula.fi/"><img src="img/logot/logo_seepsula_harmaa.png"></a>
+	<a href="http://www.okautorent.fi/"><img src="img/logot/logo_okrent_harmaa.png"></a>
 </div>
