08cb9dcfd4d62afb4362c47bdef4af04f382f4a5
Henri Pihkala <henri.pihkala@iki.fi>
2014-02-04 22:54:25 +0200

    CSS reset + added some logos

--- content/yhteistyo.php
+++ content/yhteistyo.php
@@ -4,8 +4,12 @@ header("Content-Type: text/html; charset=utf-8");
 <div id="logot">
 	<img src="img/logot/logo_espoo_harmaa.png">
 	<img src="img/logot/logo_julkku_harmaa.png">
-	<img src="img/logot/logo_metsa_harmaa.png"><br>
+	<img src="img/logot/logo_metsa_harmaa.png">
 	<img src="img/logot/logo_okrent_harmaa.png">
 	<img src="img/logot/logo_tek_harmaa.png">
 	<img src="img/logot/logo_yit_harmaa.png">
+	<img src="img/logot/logo_capgemini_harmaa.png">
+	<img src="img/logot/logo_dhl_harmaa.png">
+	<img src="img/logot/logo_roland_harmaa.png">
+	<img src="img/logot/logo_seepsula_harmaa.png">
 </div>
