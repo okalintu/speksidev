0b5ecd8f18f01e667579435fa5e926581db0c969
okalintu <ilkka.oksanen@aalto.fi>
2014-02-04 20:00:26 +0200

    added time to showtimes

--- content/macabre.php
+++ content/macabre.php
@@ -14,26 +14,27 @@ Tervetuloa lumoutumaan Danse Macabresta!
 <table id="showtimes">
     <?php 
     $shows = array(
-        array("17.3.", "Aleksanterin teatteri", "Helsinki"),
-        array("19.3.", "Hällä-teatteri", "Tampere"),
-        array("21.3.", "Pohjankartanon juhlasali", "Oulu"),
-        array("25.3.", "Aleksanterin teatteri", "Helsinki"),
-        array("24.3.", "Aleksanterin teatteri", "Helsinki"),
-        array("23.3.", "Aleksanterin teatteri", "Helsinki"),
-        array("28.3.", "Louhisali", "Espoo"),
-        array("29.3.", "Louhisali", "Espoo"),
-        array("30.3.", "Louhisali", "Espoo"),
-        array("8.4.",  "Aleksanterin teatteri", "Helsinki"),
-        array("7.4.",  "Aleksanterin teatteri", "Helsinki"),
-        array("12.4.", "Sigyn-sali", "Turku"),
-        array("14.4.", "Kaupunginteatteri", "Lappeenranta"),
+        array("17.3.", "19" ,"Aleksanterin teatteri", "Helsinki"),
+        array("19.3.", "19" ,"Hällä-teatteri", "Tampere"),
+        array("21.3.", "18" ,"Pohjankartanon juhlasali", "Oulu"),
+        array("25.3.", "19" ,"Aleksanterin teatteri", "Helsinki"),
+        array("24.3.", "19" ,"Aleksanterin teatteri", "Helsinki"),
+        array("23.3.", "19" ,"Aleksanterin teatteri", "Helsinki"),
+        array("28.3.", "19" ,"Louhisali", "Espoo"),
+        array("29.3.", "16" ,"Louhisali", "Espoo"),
+        array("30.3.", "16" ,"Louhisali", "Espoo"),
+        array("8.4.",  "19" ,"Aleksanterin teatteri", "Helsinki"),
+        array("7.4.",  "19" ,"Aleksanterin teatteri", "Helsinki"),
+        array("12.4.", "19" ,"Sigyn-sali", "Turku"),
+        array("14.4.", "18" ,"Kaupunginteatteri", "Lappeenranta"),
     );
     foreach($shows as $s) {
         echo '
         <tr>
             <td class="pvm">'.$s[0].'</td>
-            <td class="kaupunki">'.$s[2].'</td>  
-            <td class="paikka">'.$s[1].'</td>   
+            <td class="time">'.$s[1].':00</td>
+            <td class="kaupunki">'.$s[3].'</td>  
+            <td class="paikka">'.$s[2].'</td>   
         </tr>';
     }
     ?>
