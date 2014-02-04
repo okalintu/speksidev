87d16b45f5cddf57951d96dfc2a3930ae372a69e
speksi <speksi@cod.nodeta.fi>
2014-02-04 23:09:22 +0200

    added links to logos

--- content/yhteistyo.php
+++ content/yhteistyo.php
@@ -2,10 +2,10 @@
 header("Content-Type: text/html; charset=utf-8");
 ?>
 <div id="logot">
-	<img src="img/logot/logo_espoo_harmaa.png">
-	<img src="img/logot/logo_julkku_harmaa.png">
-	<img src="img/logot/logo_metsa_harmaa.png"><br>
-	<img src="img/logot/logo_okrent_harmaa.png">
-	<img src="img/logot/logo_tek_harmaa.png">
-	<img src="img/logot/logo_yit_harmaa.png">
+	<a href="http://www.espoo.fi/"><img src="img/logot/logo_espoo_harmaa.png"></a>
+	<a href="http://julkku.fi/"><img src="img/logot/logo_julkku_harmaa.png"></a>
+	<a href="http://www.metsagroup.fi/"><img src="img/logot/logo_metsa_harmaa.png"></a><br>
+	<a href="http://www.okautorent.fi/"><img src="img/logot/logo_okrent_harmaa.png"></a>
+	<a href="http://tek.fi/"><img src="img/logot/logo_tek_harmaa.png"></a>
+	<a href="http://www.yit.fi/"><img src="img/logot/logo_yit_harmaa.png"></a>
 </div>
