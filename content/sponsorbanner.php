9fada6ba53fdf67153ee9baf985ca3f4cebb46f8
okalintu <ilkka.oksanen@aalto.fi>
2014-02-05 20:30:22 +0200

    fixed julians quote and added sponsorfooter to git

---
+++ content/sponsorbanner.php
@@ -0,0 +1,6 @@
+<div id="sponsorz">
+    <h2 class="companies">Pääyhteistyökumppanit</h2>
+    <a href="http://www.espoo.fi/"><img src="img/logot/logo_espoo_harmaa.png"></a>
+    <a href="http://seepsula.fi/"><img src="img/logot/logo_seepsula_harmaa.png"></a>
+    <a href="http://www.yit.fi/"><img src="img/logot/logo_yit_harmaa.png"></a>
+</div>
